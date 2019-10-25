98bfc8bf990c353732cfbe68bcd2f3fa20b8bcac
vic30004 <victor_993@msn.com>
2019-10-25 14:25:59 -0700

    port update

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ app.use('/api/v1/auth',auth)
 app.use('/api/v1/users',users)
 app.use(errorHandler);
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env || 3000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
