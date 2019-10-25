fd160c211e1c2ab3af829b97232e512395090ba2
vic30004 <victor_993@msn.com>
2019-10-25 14:46:39 -0700

    minor update

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async ()=>{
-   const conn= await mongoose.connect(process.env.MONGO_URI,{
+   const conn= await mongoose.connect("mongodb+srv://victor123:password4321@portfolio-1ebcm.mongodb.net/portfolio?retryWrites=true&w=majority",{
        useNewUrlParser: true,
        useCreateIndex: true,
        useFindAndModify:false,
