66a7ccfae6c7f186ccd5c75a6cf76a8d07117818
vic30004 <victor_993@msn.com>
2019-11-14 19:53:34 -0800

    deploy

--- controllers/auth.js
+++ controllers/auth.js
@@ -207,7 +207,7 @@ const sendTokenResponse = (user, statusCode, res) => {
 
   const options = {
     expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+      Date.now() + 30 * 24 * 60 * 60 * 1000
     ),
     httpOnly: true
   };
